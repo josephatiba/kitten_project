762cb4fc9d9b4f2c086d092de749c997145ce1d1
Joey Atiba <joey.atiba@gmail.com>
2014-12-05 15:53:35 -0800

    Sets the background, the margins, adds in the angular filters

--- kitties-starter-app/js/KittiesController.js
+++ kitties-starter-app/js/KittiesController.js
@@ -1,31 +1,49 @@
-[
-		{
-			catName: 'Karl',
-			monthsOld: 5,
-			gender: 'boy',
-			image: "images/carl.png",
-			adopted: true,
-		},
-		{
-			catName: 'Jack',
-			monthsOld: 4,
-			gender: 'boy',
-			image: "images/jack.png",
-			adopted: false
-		},
-		{
-			catName: 'Oscar',
-			monthsOld: 2,
-			gender: 'boy',
-			image: "images/oscar.png",
-			adopted: false
-		},
-		{
-			catName: 'Princess Mew',
-			monthsOld: 3,
-			gender: 'girl',
-			image: "images/princessmew.png",
-			adopted: false
-		}
+angular
+	.module('KittenApp')
+	.controller('KittenController', KittenController);
 
-	]
+	function KittenController () {
+	
+	
+		this.addKitten = addKitten;
+		this.test = "this is a test.";
+
+		this.kittenList = [
+				{
+					catName: 'Karl',
+					monthsOld: 5,
+					gender: 'boy',
+					image: "images/carl.png",
+					adopted: true,
+				},
+				{
+					catName: 'Jack',
+					monthsOld: 4,
+					gender: 'boy',
+					image: "images/jack.png",
+					adopted: false
+				},
+				{
+					catName: 'Oscar',
+					monthsOld: 2,
+					gender: 'boy',
+					image: "images/oscar.png",
+					adopted: false
+				},
+				{
+					catName: 'Princess Mew',
+					monthsOld: 3,
+					gender: 'girl',
+					image: "images/princessmew.png",
+					adopted: false
+				}
+
+			];
+
+			this.adoptList = [];
+
+			function addKitten() {
+				this.adoptList.push({catName: this.catName, monthsOld: this.monthsOld, gender: this.gender, image: this.image, adopted: true});
+			}
+
+}
